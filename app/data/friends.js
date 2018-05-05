58aade54c546af5fb55c0679a4a593b19fcd9bfb
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-05-05 05:54:27 -0700

    revisions all around

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,8 +6,8 @@
 
 var friends = [
     {
-       "name":"Ahmed",
-       "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+       "name":"Superman",
+       "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeo9Io-geqjbXtn5wxJOXKh-s5E6y5gTq7qYi_4TB9dcy0_oM8",
        "scores":[
           "5",
           "1",
@@ -22,8 +22,8 @@ var friends = [
        ]
     },
     {
-       "name":"Jacob Deming",
-       "photo":"https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+       "name":"Wonder Woman",
+       "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeo9Io-geqjbXtn5wxJOXKh-s5E6y5gTq7qYi_4TB9dcy0_oM8",
        "scores":[
           "4",
           "2",
@@ -38,8 +38,8 @@ var friends = [
        ]
     },
     {
-       "name":"Jeremiah Scanlon",
-       "photo":"https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+       "name":"Batman",
+       "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeo9Io-geqjbXtn5wxJOXKh-s5E6y5gTq7qYi_4TB9dcy0_oM8",
        "scores":[
           "5",
           "2",
@@ -54,8 +54,8 @@ var friends = [
        ]
     },
     {
-       "name":"Louis T. Delia",
-       "photo":"https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+       "name":"Robin",
+       "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeo9Io-geqjbXtn5wxJOXKh-s5E6y5gTq7qYi_4TB9dcy0_oM8",
        "scores":[
           "3",
           "3",
@@ -70,8 +70,8 @@ var friends = [
        ]
     },
      {
-        "name":"Lou Ritter",
-        "photo":"https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+        "name":"Aquaman",
+        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeo9Io-geqjbXtn5wxJOXKh-s5E6y5gTq7qYi_4TB9dcy0_oM8",
         "scores":[
            "4",
            "3",
@@ -86,8 +86,8 @@ var friends = [
         ]
      },
      {
-        "name":"Jordan Biason",
-        "photo":"https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+        "name":"Green Lantern",
+        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeo9Io-geqjbXtn5wxJOXKh-s5E6y5gTq7qYi_4TB9dcy0_oM8",
         "scores":[
            "4",
            "4",
